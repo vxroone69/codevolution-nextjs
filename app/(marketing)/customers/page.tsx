5ff86418eccda1b4268df6f34fd8e5c9bbb3dbf9
vxroone69 <mpkvarun69@gmail.com>
2026-02-14 17:40:09 +0530

    Implement Nested layouts

---
+++ app/(marketing)/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function customers() {
+    return (
+        <div>
+            <h1>Customers Page</h1>
+            <p>This is the customers page of our Next.js application.</p>
+        </div>
+    );
+}
---
+++ app/(marketing)/revenue/page.tsx
@@ -0,0 +1,8 @@
+export default function revenue() {
+    return (
+        <div>
+            <h1>Revenue Page</h1>
+            <p>This is the revenue page of our Next.js application.</p>
+        </div>
+    );
+}
