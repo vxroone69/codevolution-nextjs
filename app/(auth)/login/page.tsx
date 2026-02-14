7b174eed5c3994ff58125d50ca213fe43dbb1dc6
vxroone69 <mpkvarun69@gmail.com>
2026-02-14 17:05:00 +0530

    Auth Groups

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+export default function login() {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <p>This is the login page of our Next.js application.</p>
+        </div>
+    );
+}
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,8 @@
+export default function register() {
+    return (
+        <div>
+            <h1>Register Page</h1>
+            <p>This is the register page of our Next.js application.</p>
+        </div>
+    );
+}
